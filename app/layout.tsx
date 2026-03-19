b370623c0b28b02cc713b56aaab194ea5657aba0
xDarey <oluwadare.emmanuel15@gmail.com>
2026-03-19 16:33:08 +0100

    retouch

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,19 +28,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
         url: '/davisicon.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    
   },
   keywords: ['luxury cars', 'automotive brokerage', 'rare vehicles', 'high performance'],
 }
